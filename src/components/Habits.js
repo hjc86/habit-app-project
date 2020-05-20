39010da57ad2e705bd301606c63365603847df5e
Medyen <medianak@hotmail.co.uk>
2020-05-20 11:35:15 +0100

    all tasks done

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -1,27 +1,31 @@
 import React from "react";
 import Button from 'react-bootstrap/Button'
 import UpdateModal from './UpdateModal'
+import CompleteModal from './CompleteModal'
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 class Habits extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             userID: null,
-            modalShow : false
+            modalShow : false,
+            modalShow1 : false
         }
     }
-
-    handleClickDelete = async (event) =>{
-        event.preventDefault();
-        const url = 'http://localhost:3001/habits';
-        const response = await fetch(url, {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({id: this.props.data.id})
-        })
-        this.props.updateState();
-    }
     
+    componentDidMount() {
+        // let completed;
+        // this.props.data.current_value / this.props.data.target_value > 1 ? completed = 1 : completed = 0;
+        // const url = 'http://localhost:3001/habits';
+        // const response = await fetch(url, {
+        //     method: 'put',
+        //     headers: {'Content-Type': 'application/json'},
+        //     body: JSON.stringify({id: this.props.data.id, completed: completed})
+        // })        
+        // this.props.updateState();
+    }
+
     handleClickUpdate = async (event) =>{
         event.preventDefault();
         console.log(event.target);
@@ -62,21 +66,34 @@ class Habits extends React.Component {
     }
 
     render () {
-        
+        const now = parseInt((this.props.data.current_value/this.props.data.target_value)*100);
+        //const progressInstance = <ProgressBar now={now} label={`${now}%`} srOnly />;
         return (
             <div className="habit-box">
                 
                 {/* <h2>{this.props.data.user_id}</h2> */}
                 <h2>{this.props.data.habit_name}</h2>
-                <h2>{this.props.data.id}</h2>
-                <h2>{this.props.data.completed}</h2>
-                <Button onClick={this.handleClickDelete}>Delete</Button>
-                <Button onClick={this.handleClickComplete}>Complete</Button>
-                <Button onClick={() => this.setState({ modalShow: true })}> Update!</Button>
+                <h2>Habit ID: {this.props.data.id}</h2>
+                <h2>Completed: {this.props.data.completed}</h2>
+                <h2>Streak: {this.props.data.streak}</h2>
 
-                <UpdateModal
+
+                
+                <Button onClick={this.handleClickComplete}>Complete</Button>
+                <Button onClick={() => this.setState({ modalShow1: true })}> Complete!!</Button>
+                <Button onClick={() => this.setState({ modalShow: true })}> Update!</Button>
+                <ProgressBar striped now={now} label={`${now}%`} srOnly />
+                 <UpdateModal
                 show={this.state.modalShow}
                 onHide={() => this.setState({modalShow : false})}
+                user_id = {this.props.userID}
+                updateState = {this.props.updateState}
+                data = {this.props.data}
+                />
+
+                <CompleteModal
+                show={this.state.modalShow1}
+                onHide={() => this.setState({modalShow1 : false})}
                 user_id = {this.props.userID}
                 updateState = {this.props.updateState}
                 data = {this.props.data}

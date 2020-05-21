02b4a9d2b10e9e9ecf8b871d6ac5d99f7ac7e1bb
Helder Carmen <heldercarmen@gmail.com>
2020-05-21 14:11:55 +0100

    can test routes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const db = require("./db/queries.js");
 
-
 server.use(express.static(path.join(__dirname, 'build')));
 // server.use(cors());
 server.use(bodyParser.json());
@@ -17,27 +16,6 @@ server.listen(process.env.PORT || 3001);
 
 
 server.use('/', index)
-
-
-
-// server.get('/users', (req,res)=> {
-//     return res.send("hello")
-// })
-
-// server.get('/users', function (req, res, next){
-//     //let users = "hello"
-//     db.getAllUsers()
-//     .then(res.send("success"))
-    
-//     // .then(function(users){
-//     //     res.status(200).json(users);
-//     // })
-//     // .catch(function(error){
-//     //     next(error);
-//     // });
-// })
-
-// server.listen(process.env.PORT || 3001);
 
 
 server.get('/ping', (req,res) => {

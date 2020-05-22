85f6215eefab1e9d19b955c4635e6a8433da7260
richard <richard.deroure@gmail.com>
2020-05-22 09:30:37 +0100

    basic log in tests

--- src/tests/LogIn.test.js
+++ src/tests/LogIn.test.js
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 import { shallow, mount } from 'enzyme';
 import LogIn from '../components/LogIn';
 
-describe('LogIn', () => {
 
+describe('LogIn', () => {
 
     let wrapper;
     beforeEach(()=>{
@@ -18,23 +18,8 @@ describe('LogIn', () => {
     it('renders login form', () => {
         expect(wrapper.find('.form').exists()).toBe(true);
     })
+  
 
-    
-    it('On log in with correct data the userID should be 1', () => {
-        const wrapper = mount(<LogIn/>);
-        const button = wrapper.find('#login-btn').at(0);
-        const fakeEvent = { preventDefault: () => console.log('preventDefault') };
-
-        wrapper.setState({username: 'test', password: '123'});
-        expect(button.length).toEqual(1);
-        button.props().onClick(fakeEvent);
-        console.log(wrapper.state());
-
-        expect(wrapper.state('userID')).toEqual(1);
-        expect(wrapper.find('#login-btn')).toBeTruthy();
-
-    })
-    
     test('Username check', () => {
         wrapper.find('.input').at(0).simulate('change', {target: {name: 'username', value: 'test'}});
         expect(wrapper.state('username')).toEqual('test');
@@ -47,19 +32,6 @@ describe('LogIn', () => {
         
         })
 
-    // it('login check with right data',()=>{
-    //     wrapper.find('.input').at(0).simulate('change', {target: {name: 'username', value: 'test'}});
-    //     wrapper.find('.input').at(1).simulate('change', {target: {name: 'password', value: '123'}});
-    //     wrapper.find('.button').at(0).simulate('click');;
-    //     expect(wrapper.state(userID)).toBe(1);
-    //     })
-        
-    // it('expectLoginFunction to be called',()=>{
-    //     wrapper.find('.input').at(0).simulate('change', {target: {name: 'username', value: 'test'}});
-    //     wrapper.find('.input').at(1).simulate('change', {target: {name: 'password', value: '123'}});
-    //     wrapper.find('.button').at(0).simulate('click');;
-    //     expect(handleClickLogIn()).toHaveBeenCalled();
-    //     })
 
 })
 
